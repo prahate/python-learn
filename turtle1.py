9466fdfa222292b19a4dac2f94e0765b11ca25b8
prathameshs@spanidea.com <prathameshs@spanidea.com>
2017-07-05 15:28:04 +0530

    Modified to genrate circle out of squares

--- turtle1.py
+++ turtle1.py
@@ -4,7 +4,7 @@ def draw_square(some_turtle):
 	for i in range(4):
 		some_turtle.forward(100)
 		some_turtle.right(90)
-	
+
 def draw_art():
 	window = turtle.Screen()
 	window.bgcolor("red")
@@ -12,12 +12,15 @@ def draw_art():
 	brad = turtle.Turtle()
 	brad.shape("turtle")
 	brad.color("yellow")
-	draw_square(brad)
+
+	for i in range(72):
+		draw_square(brad)
+		brad.right(5)
 	
-	brad1 = turtle.Turtle()
-	brad1.shape("classic")
-	brad1.color("blue")
-	brad1.circle(100)
+	#brad1 = turtle.Turtle()
+	#brad1.shape("classic")
+	#brad1.color("blue")
+	#brad1.circle(100)
 
 	window.exitonclick()
 
